38a6811dd7173fdda4591e38ffac4c441ec8b652
TmknK <mt186227@s.utsunomiya-u.ac.jp>
2019-07-13 15:21:00 +0900

    change step reference values

--- getMotorXYreference.ino
+++ getMotorXYreference.ino
@@ -12,8 +12,8 @@ int getMotorYreference(int step){
 }
 int genStepSignal(int step, int squarePeriod){
   if (step > squarePeriod/2){
-    return 8000;
+    return 4000;
   }else{
-    return 9000;
+    return 5000;
   }
 }
